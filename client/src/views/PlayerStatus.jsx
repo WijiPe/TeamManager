aa9e1060fe232437fe8b8e31ae9e05a0fab6f0e8
Wiji <peawseaw@gmail.com>
2022-01-27 20:34:54 -0800

    commit

--- client/src/views/PlayerStatus.jsx
+++ client/src/views/PlayerStatus.jsx
@@ -3,20 +3,17 @@ import Tap from '../components/Tap';
 import PlayerStatusTable from '../components/PlayerStatusTable';
 
 
-const PlayerStatus = (props) => {
+const PlayerStatus = () => {
 
     const [tap, setTap] = useState("")
     const [index, setIndex] = useState(null)
-    const [refresh, setRefresh] = useState(false)
 
     const findGame =(newTap, index) =>{
         setTap(newTap)
         setIndex(index)
     }
 
-    // const reload1 = () =>{
-    //     setRefresh(!refresh)
-    // }
+
 
     return (
         <div>
